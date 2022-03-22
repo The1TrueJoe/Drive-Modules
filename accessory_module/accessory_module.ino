aaa16db6d903a1a4bea6fe4d39452fc55e04bf17
The1TrueJoe <jttelaak@hotmail.com>
2022-03-22 04:37:34 -0400

    Beta of accessory module

--- accessory_module/accessory_module.ino
+++ accessory_module/accessory_module.ino
@@ -1,17 +1,43 @@
+/**
+ * @file accessory_module.ino
+ * 
+ * @author Joseph Telaak
+ * 
+ * @brief Accessory control module for the golf cart
+ * 
+ * @version 0.1
+ * @date 2022-03-22
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
 #include "module.h"
 
-// --------- Pin Definitions
+// Relays
 
-#define RIGHT_TAIL_RELAY 1
-#define LEFT_TAIL_RELAY 2
-#define HEAD_LIGHT_RELAY 3
-#define HORN_RELAY 4
+#define LEFT_TAIL_RELAY 8
+#define RIGHT_TAIL_RELAY 7
+#define TAIL_LIGHT_RELAY 6
+#define HEAD_LIGHT_RELAY 5
+#define REAR_BUZZ_RELAY 4
+#define HORN_RELAY 9
 
 // Relay IDs
 const uint8_t right_tail_id = 0x00;
 const uint8_t left_tail_id = 0x01;
 const uint8_t head_light_id = 0x02;
-const uint8_t horn_relay_id = 0x03;
+const uint8_t tail_light_id = 0x03;
+const uint8_t horn_id = 0x04;
+const uint8_t rear_buzz_id = 0x05;
+
+// Brake Pedal
+#define BRAKE_PEDAL 3
+
+// Time
+bool continue_loop = true;
+int def_blinking_interval = 2000;
+int def_horn_interval = 50;
 
 // --------- Setup
 
@@ -24,33 +50,18 @@ void setup() {
     // Standard module setup
     standardModuleSetup();
 
+    // Announce Ready
+    ready();
+    holdTillEnabled();
+
+    // Setup Interupts
+    attachInterupt(CAN_INT, canLoop, FALLING);
+    attachInterupt(BRAKE_PEDAL, pedalPressed, RISING);
+
     // Relay setup
     setupRelays();
 
 }
-
-/**
- * @brief Setup the relays
- * 
- */
-
-void setupRelays() {
-    // Set pinmode
-    Serial.println("Relays: Setting up Relay Control Pins");
-    pinMode(RIGHT_TAIL_RELAY, OUTPUT);
-    pinMode(LEFT_TAIL_RELAY, OUTPUT);
-    pinMode(HEAD_LIGHT_RELAY, OUTPUT);
-    pinMode(HORN_RELAY, OUTPUT);
-
-    // Reset relays
-    resetRelays();
-
-    // Setup complete
-    Serial.println("Relays: Setup Complete");
-
-}
-
-// --------- Main Loop
 
 /**
  * @brief Main loop
@@ -62,11 +73,88 @@ void loop() {
         standardModuleLoopHead();
 
         switch (can_msg_in.data[0]) {
-            // 
-            case 0x0C:
-                relayControlSequence(); break;
+            case 0x0A:
+                switch (can_msg_in.data[2]) {
+                    case 0x01:
+                        openRelay(can_msg_in.data[1]);
+                        break;
+
+                    case 0x02:
+                        closeRelay(can_msg_in.data[1]);
+                        continue_loop = false;
+                        break;
+
+                    default:
+                        break;
+
+                }
+
+                break;
                 
-        
+
+            case 0x0B:
+                switch (can_msg_in.data[1]) {
+                    case 0x0B:
+                        int interval = (can_msg_in.data[3] != 0x00) ? ((can_msg_in.data[3] << 8) * (can_msg_in.data[4] << 8)) : def_blink_interval;
+                        int id = can_msg_in.data[2];
+
+                        while (continue_loop) {
+                            closeRelay(id);
+                            delay(interval);
+                            openRelay(id);
+
+                        }
+
+                        break;
+
+
+                    case 0x01:
+                        int interval = (can_msg_in.data[3] != 0x00) ? ((can_msg_in.data[3] << 8) * (can_msg_in.data[4] << 8)) : def_horn_interval;
+
+                        closeRelay(horn_id);
+                        delay(interval);
+                        openRelay(horn_id);
+
+                        break;
+                        
+                    default:
+                        break;
+
+                }
+
+                break;
+
+
+            case 0x0C:
+                switch (can_msg_in.data[1]) {
+                    case 0x0A:
+                        postRelayStatus(can_msg_in.data[2]);
+                        break;
+
+                    case 0x0E:
+                        switch (can_msg_in.data[2]) {
+                            case 0x01:
+                                def_blink_interval = (can_msg_in.data[3] != 0x00) ? ((can_msg_in.data[3] << 8) * (can_msg_in.data[4] << 8)) : def_blink_interval;
+                                break;
+
+                            case 0x02:
+                                def_horn_interval = (can_msg_in.data[3] != 0x00) ? ((can_msg_in.data[3] << 8) * (can_msg_in.data[4] << 8)) : def_horn_interval;
+                                break;
+
+                            default:
+                                break;
+                                
+                        }
+
+                        break;
+
+                    default:
+                        break;
+
+                }
+
+                break;
+                
             default:
                 break;
 
@@ -78,58 +166,52 @@ void loop() {
 
 }
 
-/**
- * @brief Relay control sequence
- * 
- */
+// --------- Pedals 
 
-void relayControlSequence() {
-    uint8_t relay_id = can_msg_in.data[1];
+/** @brief When pedal is pressed */
+void pedalPressed() {
+    closeRelay(tail_light_id);
+    attachInterupt(BRAKE_PEDAL, pedalReleased, FALLING);
 
-    switch (can_msg_in.data[2]) {
-        case 0x0A:
-            switch (can_msg_in.data[3]) {
-                case 0x00:
-                    closeRelay(relay_id); break;
-                case 0x01:
-                    openRelay(relay_id); break;
-                case 0x02:
-                    postRelayStatus(relay_id); break;
-                default:
-                    invalidCommand(); break;
+}
 
-            }
-            
-            break;
-
-        case 0x0C:
-            switch (can_msg_in.data[3]) {
-                case 0x00:
-                    openRelay(relay_id); break;
-                case 0x01:
-                    closeRelay(relay_id); break;
-                default:
-                    invalidCommand(); break;
-
-            }
-
-            break;
-
-        default:
-            invalidCommand(); break;
-
-    }
+/** @brief When pedal is released */
+void pedalReleased() {
+    openRelay(tail_light_id);
+    attachInterupt(BRAKE_PEDAL, pedalPressed, RISING);
 
 }
 
 // --------- Relay control
 
+/** @brief Setup the relays  */
+void setupRelays() {
+    // Set pinmode
+    Serial.println("Relays: Setting up Relay Control Pins");
+    pinMode(RIGHT_TAIL_RELAY, OUTPUT);
+    pinMode(LEFT_TAIL_RELAY, OUTPUT);
+    pinMode(TAIL_LIGHT_RELAY, OUTPUT);
+    pinMode(HEAD_LIGHT_RELAY, OUTPUT);
+    pinMode(REAR_BUZZ_RELAY, OUTPUT);
+    pinMode(HORN_RELAY, OUTPUT);
+
+    // Reset relays
+    resetRelays();
+
+    // Setup complete
+    Serial.println("Relays: Setup Complete");
+
+}
+
 /** @brief Reset all relays */
 void resetRelays() {
-    for (uint8_t i = 0x00; i < 0x04; i++) {
-        closeRelay(i);
+    closeRelay(right_tail_id);
+    closeRelay(left_tail_id);
+    closeRelay(tail_light_id);
+    closeRelay(head_light_id);
+    closeRelay(horn_id);
+    closeRelay(rear_buzz_id);
 
-    }
 }
 
 /**
@@ -150,6 +232,11 @@ void closeRelay(uint8_t id) {
             digitalWrite(LEFT_TAIL_RELAY, LOW); 
             break;
 
+        case tail_light_id:
+            Serial.println("Head Light Relay: Close");
+            digitalWrite(TAIL_LIGHT_RELAY, LOW);
+            break;
+
         case head_light_id:
             Serial.println("Head Light Relay: Close");
             digitalWrite(HEAD_LIGHT_RELAY, LOW); 
@@ -160,10 +247,18 @@ void closeRelay(uint8_t id) {
             digitalWrite(HORN_RELAY, LOW); 
             break;
 
+        case rear_buzz_id:
+            Serial.println("Rear Buzzer Relay: Close");
+            digitalWrite(REAR_BUZZ_RELAY, LOW);
+            break;
+
         default:
-            invalidCommand(); break;
+            break;
 
     }
+
+    postRelayStatus(id);
+
 }
 
 /**
@@ -184,6 +279,11 @@ void openRelay(uint8_t id) {
             digitalWrite(LEFT_TAIL_RELAY, HIGH); 
             break;
 
+        case tail_light_id:
+            Serial.println("Head Light Relay: Open");
+            digitalWrite(TAIL_LIGHT_RELAY, HIGH);
+            break;
+
         case head_light_id:
             Serial.println("Head Light Relay: Open");
             digitalWrite(HEAD_LIGHT_RELAY, HIGH); 
@@ -194,10 +294,18 @@ void openRelay(uint8_t id) {
             digitalWrite(HORN_RELAY, HIGH); 
             break;
 
+        case rear_buzz_id:
+            Serial.println("Rear Buzzer Relay: Open");
+            digitalWrite(REAR_BUZZ_RELAY, HIGH);
+            break;
+
         default:
-            invalidCommand(); break;
+            break;
 
     }
+
+    postRelayStatus(id);
+
 }
 
 /**
@@ -212,27 +320,24 @@ void openRelay(uint8_t id) {
 bool checkRelay(uint8_t id) {
     switch (id) {
         case right_tail_id:
-            bool pos = (digitalRead(RIGHT_TAIL_RELAY) == 1);
-            Serial.println("Right Tail Light Relay: Currently " + (pos ? "Open" : "Closed"));
-            return pos;
+            return digitalRead(RIGHT_TAIL_RELAY) == HIGH;
 
         case left_tail_id:
-            bool pos = (digitalRead(LEFT_TAIL_RELAY) == 1);
-            Serial.println("Left Tail Light Relay: Currently " + (pos ? "Open" : "Closed"));
-            return pos;
+            return digitalRead(LEFT_TAIL_RELAY) == HIGH;
+
+        case tail_light_id:
+            return digitalRead(TAIL_LIGHT_RELAY) == HIGH;
 
         case head_light_id:
-            bool pos = (digitalRead(HEAD_LIGHT_RELAY) == 1);
-            Serial.println("Head Light Relay: Currently " + (pos ? "Open" : "Closed"));
-            return pos;
+            return digitalRead(HEAD_LIGHT_RELAY) == HIGH;
 
         case horn_relay_id:
-            bool pos = (digitalRead(HORN_RELAY) == 1);
-            Serial.println("Horn Relay: Currently " + (pos ? "Open" : "Closed"));
-            return pos;
+            return digitalRead(HORN_RELAY) == HIGH;
         
+        case rear_buzz_id:
+            return digitalRead(REAR_BUZZ_RELAY) == HIGH;
+
         default:
-            invalidCommand();
             return false;
 
     }
@@ -245,9 +350,11 @@ bool checkRelay(uint8_t id) {
  */
 
 void postRelayStatus(uint8_t id) {
-    uint8_t status = checkRelay(id) ? 0x01 : 0x00;
-    uint8_t message[can_msg_in.dlc] = { 0x0C, id, 0x0A, 0x02, status, 0x00, 0x00, 0x00 };
+    // Build Message
+    uint8_t status = checkRelay(id) ? 0x01 : 0x02;
+    uint8_t message[can_msg_in.dlc] = { 0x0C, id, 0x0A, status, 0x02, 0x00, 0x00, 0x00 };
 
+    // Send Message
     sendCANMessage(master_can_id, message);
 
 }
