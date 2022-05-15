7718e140bb3dd920bed592af51b6f9b633830f65
The1TrueJoe <jttelaak@hotmail.com>
2022-05-15 17:04:48 -0400

    Change update script

--- revision_1/arduino_lib_update.sh
+++ revision_1/arduino_lib_update.sh
@@ -2,36 +2,6 @@
 # Only temporary until another solution is figured out
 
 # Go to libarary directory
-cd ~/Documents/Arduino/libraries/
-
-# Checkout only module code
-svn checkout https://github.com/The1TrueJoe/Drive-Modules/trunk/revision_1/std_modules
-
-# Go into the new folder and include full submodule repos
-cd std_modules
-
-if [ -d "arduino-mcp2515" ]; then
-    cd arduino-mcp2515
-    git pull
-    cd ..
-else 
-    git clone https://github.com/GSSM-AutoGolfCart/arduino-mcp2515
-fi
-
-if [ -d "arduino-mcp4xxx" ]; then
-    cd arduino-mcp2515
-    git pull
-    cd ..
-else 
-    git clone https://github.com/GSSM-AutoGolfCart/arduino-mcp4xxx
-fi
-
-if [ -d "ArduinoCore-avr" ]; then
-    cd ArduinoCore-avr
-    git pull
-    cd ..
-else 
-    git clone https://github.com/GSSM-AutoGolfCart/ArduinoCore-avr
-fi
+cp -R -f libs/ ~/Documents/arduino/libraries
 
 echo "Done!"
