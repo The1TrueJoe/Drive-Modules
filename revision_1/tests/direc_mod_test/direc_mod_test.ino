ae03c9a82c497aa29744d44e85100a5a74ee33db
The1TrueJoe <jttelaak@hotmail.com>
2022-05-19 15:49:58 -0400

    Both enable pins test

--- revision_1/tests/direc_mod_test/direc_mod_test.ino
+++ revision_1/tests/direc_mod_test/direc_mod_test.ino
@@ -5,8 +5,10 @@
 #ifdef TEST_STEERING
     // Steering Motor Ctrl
     #define STR_L_PWM 6
+    #define STR_L_ENABLE 4
+
     #define STR_R_PWM 9
-    #define STR_ENABLE 4
+    #define STR_R_ENABLE 0
 
     // Steering Linear Actuator Potentiometer
     #define STR_POT A5
@@ -27,8 +29,10 @@
 #ifdef TEST_BRAKES
     // Brake Motor Ctrl
     #define BRK_L_PWM 5
+    #define BRK_L_ENABLE 7
+
     #define BRK_R_PWM 10
-    #define BRK_ENABLE 7
+    #define BRK_R_ENABLE 1
 
     // Brake Actuator Potentiometer
     #define BRK_POT A4
@@ -41,39 +45,46 @@ void setup() {
     delay(2000);
 
     #ifdef TEST_STEERING
-        pinMode(STR_ENABLE, OUTPUT);
+        pinMode(STR_L_ENABLE, OUTPUT);
+        pinMode(STR_R_ENABLE, OUTPUT);
         pinMode(STR_L_PWM, OUTPUT);
         pinMode(STR_R_PWM, OUTPUT);
         pinMode(STR_POT, INPUT);
 
-        digitalWrite(STR_ENABLE, LOW);
+        digitalWrite(STR_L_ENABLE, LOW);
+        digitalWrite(STR_R_ENABLE, LOW);
         digitalWrite(STR_L_PWM, LOW);
         digitalWrite(STR_R_PWM, LOW);
 
         Serial.println("Running L PWM");
 
-        digitalWrite(STR_L_PWM, 255);
-        digitalWrite(STR_R_PWM, LOW);
-        digitalWrite(STR_ENABLE, HIGH);
+        digitalWrite(STR_R_ENABLE, LOW);
+        analogWrite(STR_R_PWM, 0);
+
+        analogWrite(STR_L_PWM, 255);
+        digitalWrite(STR_L_ENABLE, LOW);
         
         for (int i = 0; i < 50; i++) {
             Serial.println("Pot Reading: " + String(analogRead(STR_POT)));
             delay(100);
         }
 
-        digitalWrite(STR_ENABLE, LOW);
+        digitalWrite(STR_L_ENABLE, LOW);
+        analogWrite(STR_L_PWM, 0);
+
         Serial.println("Running R PWM");
 
-        digitalWrite(STR_L_PWM, LOW);
-        digitalWrite(STR_R_PWM, 255);
-        digitalWrite(STR_ENABLE, HIGH);
+        analogWrite(STR_R_PWM, 255);
+        digitalWrite(STR_R_ENABLE, LOW);
         
         for (int i = 0; i < 50; i++) {
             Serial.println("Pot Reading: " + String(analogRead(STR_POT)));
             delay(100);
         }
 
-        digitalWrite(STR_ENABLE, LOW);
+        digitalWrite(STR_R_ENABLE, LOW);
+        analogWrite(STR_R_PWM, 0);
+
         Serial.println("Done");
 
     #endif
@@ -81,39 +92,46 @@ void setup() {
     #ifdef TEST_BRAKES
         Serial.println("Testing Brakes");
 
-        pinMode(BRK_ENABLE, OUTPUT);
+        pinMode(BRK_L_ENABLE, OUTPUT);
+        pinMode(BRK_R_ENABLE, OUTPUT);
         pinMode(BRK_L_PWM, OUTPUT);
         pinMode(BRK_R_PWM, OUTPUT);
         pinMode(BRK_POT, INPUT);
 
-        digitalWrite(BRK_ENABLE, LOW);
+        digitalWrite(BRK_L_ENABLE, LOW);
+        digitalWrite(BRK_R_ENABLE, LOW);
         digitalWrite(BRK_L_PWM, LOW);
         digitalWrite(BRK_R_PWM, LOW);
 
         Serial.println("Running L PWM");
 
-        digitalWrite(BRK_L_PWM, 255);
-        digitalWrite(BRK_R_PWM, LOW);
-        digitalWrite(BRK_ENABLE, HIGH);
+        digitalWrite(BRK_R_ENABLE, LOW);
+        analogWrite(BRK_R_PWM, 0);
+
+        analogWrite(BRK_L_PWM, 255);
+        digitalWrite(BRK_L_ENABLE, LOW);
         
         for (int i = 0; i < 50; i++) {
             Serial.println("Pot Reading: " + String(analogRead(BRK_POT)));
             delay(100);
         }
 
-        digitalWrite(BRK_ENABLE, LOW);
+        digitalWrite(BRK_L_ENABLE, LOW);
+        analogWrite(BRK_L_PWM, 0);
+
         Serial.println("Running R PWM");
-        
-        digitalWrite(BRK_L_PWM, LOW);
-        digitalWrite(BRK_R_PWM, 255);
-        digitalWrite(BRK_ENABLE, HIGH);
+
+        analogWrite(BRK_R_PWM, 255);
+        digitalWrite(BRK_R_ENABLE, LOW);
         
         for (int i = 0; i < 50; i++) {
             Serial.println("Pot Reading: " + String(analogRead(BRK_POT)));
             delay(100);
         }
 
-        digitalWrite(BRK_ENABLE, LOW);
+        digitalWrite(BRK_R_ENABLE, LOW);
+        analogWrite(BRK_R_PWM, 0);
+        
         Serial.println("Done");
 
     #endif
